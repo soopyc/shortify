ad78f2acfaf9a6ed37791f4c84b43f1794c1c294
Sophie <me@soopy.moe>
2022-06-19 02:29:29 +0800

    chore: that commit doesn't even include the layout

--- migrations/1655476902623_change-deletion-token-to-access-token.cjs
+++ migrations/1655476902623_change-deletion-token-to-access-token.cjs
@@ -1,12 +1,5 @@
 /* eslint-disable camelcase */
 
-
-exports.shorthands = undefined;
-
-/**
- * ↓ doesn't even work
- * @param {import("node-pg-migrate").MigrationBuilder} pgm 
- */
 exports.up = pgm => {
     pgm.renameColumn('links', 'deletionToken', 'accessToken')
 };
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import "../app.scss";
+    import { dev } from "$app/env";
+    import { session } from "$app/stores";
+    export const pkg = $session.pkg;
+
+    export const devMode = {
+        color: dev ? "text-danger" : "text-success",
+        text: dev ? "dev" : "prod",
+    };
+
+    // // @ts-ignore shut up typescirpt
+    // export const pkg: any = JSON.parse(process.env.PACKAGE_JSON);
+</script>
+
+<nav class="app-nav">
+    <p>
+        <a href="/" class="text-reset">
+            <span class="text-muted">Linkify</span>
+        </a>
+        <a href="//koakuma.soopy.moe/gensokyo/link-shortener" title="Source code repository" class="text-reset">
+            <span class="text-primary">v{pkg.version}</span>
+        </a>
+        (<span class={devMode.color}>{devMode.text}</span>)
+    </p>
+
+    <div class="d-flex gap-3">
+        <a href="/" class="text-reset text-decoration-none">Home</a>
+        <a href="/terms" class="text-reset text-decoration-none">Terms</a>
+    </div>
+</nav>
+<div class="app"><slot /></div>
