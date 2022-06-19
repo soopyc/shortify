2dc51fd8f0581044b5d709d0903e9d7b3dacb428
Sophie <me@soopy.moe>
2022-06-20 00:43:09 +0800

    chore: s(a|c)ss shenanigans

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import "../app.scss";
+    import "../app.sass";
     import { dev } from "$app/env";
     import { session } from "$app/stores";
     export const pkg = $session.pkg;
