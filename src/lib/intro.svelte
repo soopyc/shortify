19a85eceb003275482639e36cf88962af01ccd41
Cassie Cheung <me@soopy.moe>
2023-07-09 19:58:25 +0800

    component: add intro component

---
+++ src/lib/intro.svelte
@@ -0,0 +1,12 @@
+<h1>Welcome to nginx!</h1>
+<p>
+    If you see this page, the nginx web server is successfully installed and working.
+    Further configuration is required.
+</p>
+<p>
+    For online documentation and support please refer to <a href="//nginx.org">nginx.org</a>.
+</p>
+<p>
+    The webpage administrator told us not to advertise our commercial service. :(
+</p>
+<p><i>Thank you for using nginx.</i></p>
