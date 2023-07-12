df3c1e70bebe2fb9d2f61491a41f7c23ab14f3b1
Cassie Cheung <me@soopy.moe>
2023-07-12 14:08:56 +0800

    feat: add index page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Intro from "$lib/intro.svelte";
+    import ShortenInput from "$lib/shortenInput.svelte";
+</script>
+
+<ShortenInput />
+<Intro /> <!-- Modify /src/lib/intro.svelte to your liking! -->
