4fb0c74b3155143bbf641999f14f1bf6e51b0fc4
Cassie Cheung <me@soopy.moe>
2024-01-23 14:41:20 +0800

    chore: config vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import Icons from "unplugin-icons/vite";
 import _package from "./package.json";
 
 export default defineConfig({
+	build: {
+		target: "esnext"
+	},
 	plugins: [
 		sveltekit(),
 		Icons({
 			compiler: "svelte",
 			autoInstall: true,
+			defaultStyle: "vertical-align: middle; text-align: center;",
 		}),
 	],
 	define: {
