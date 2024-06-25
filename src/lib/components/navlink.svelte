f77288c0958fc388a80e55785c68dca62a3dbd9d
Cassie Cheung <me@soopy.moe>
2024-06-25 22:56:56 +0800

    components(navlink): overhaul design to be simpler with unocss

--- src/lib/components/navlink.svelte
+++ src/lib/components/navlink.svelte
@@ -1,40 +1,9 @@
-<script lang="ts">
+<script>
 	import { page } from "$app/stores";
-	export let name: string;
-	export let href: string;
+	export let href;
+	export let name;
 </script>
 
-<a {href} class:current={$page.url.pathname == href}>
+<a {href} class="underline" class:underline-dotted={$page.url.pathname == href}>
 	{name}
 </a>
-
-<style lang="less">
-	@import "$lib/globals";
-
-	a {
-		padding-top: 3px;
-		padding-bottom: 1px;
-		transition-property: padding, color, background-color;
-		transition-duration: 250ms;
-
-		// TODO: decide on whether we want this or not
-		// @media screen and (max-width: @mobile-breakpoint) {
-		@media (prefers-reduced-motion) {
-			padding-right: 10px;
-		}
-
-		&:visited,
-		&:active,
-		& {
-			color: unset; // var(--primary);
-		}
-		&:hover,
-		&:focus,
-		&.current {
-			padding-left: 10px;
-			padding-right: 10px;
-			color: var(--secondary);
-			background-color: var(--primary);
-		}
-	}
-</style>
