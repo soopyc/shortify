5c5be43f9269216d86ac6b195a64b905df1b7431
Cassie Cheung <me@soopy.moe>
2024-06-25 22:57:02 +0800

    components(nav): overhaul design

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,62 +1,23 @@
-<script lang="ts">
-    import Navlink from "./navlink.svelte";
-    import { env } from "$env/dynamic/public";
+<script>
+	import { PUB_APP_NAME } from "$env/static/public";
+	import Navlink from "./navlink.svelte";
 
-    export const app_version = APP_VER
-    export const app_name = env.PUB_APP_NAME;
+    export const app_version = APP_VER // @eslint-silent-error this is defined in app.d.ts.
 </script>
 
-<nav>
-    <div id="branding" class="colToRow">
-        <a href="/">
-            <img class:invert={env.PUB_MONOCHROME_ICON} id="banner" src="/banner.png" alt="Banner for this website, {app_name}"/>
-        </a>
-        <span>v{app_version}</span>
-    </div>
+<div class="flex flex-auto place-content-between">
+	<div id="app_name">
+		<span>{PUB_APP_NAME}</span>
+		<span>v{app_version}</span>
+	</div>
 
-    <div id="links" class="colToRow">
-        <Navlink name="Home" href="/" />
-        <Navlink name="Docs" href="/docs" />
-        <Navlink name="About" href="/about" />
-    </div>
-</nav>
+	<div id="login">
+		<span>Log in</span>
+	</div>
+</div>
 
-<style lang="less">
-	@import "$lib/globals";
-	
-    nav {
-        font-size: 1rem;
-        margin: 1rem 0;
-
-        display: flex;
-        align-items: center;
-        justify-content: space-around;
-    }
-
-	.colToRow {
-		display: grid;
-		align-items: end;
-		justify-items: center;
-		gap: 0.5em;
-		grid-auto-flow: column;
-		
-		@media screen and (max-width: @mobile-breakpoint) {
-			justify-items:end;
-			grid-auto-flow: unset;
-			grid-auto-columns: minmax(auto, 8em);
-		}
-	}
-
-    #banner {
-        height: 3em;
-    }
-
-    .invert {
-        @media (prefers-color-scheme: light) {
-            filter: none;
-        };
-        @media (prefers-color-scheme: dark) {
-            filter: invert(1);
-        }
-    }
-</style>
+<div id="links" class="mt-2 flex gap-2 justify-end">
+	<Navlink name="Home" href="/" />
+	<Navlink name="Docs" href="/docs" />
+	<Navlink name="About" href="/about" />
+</div>
