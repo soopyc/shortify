55085c45a59a451725cab37cd13f638da1ba6600
Cassie Cheung <me@soopy.moe>
2024-06-25 22:55:05 +0800

    components(code): init

---
+++ src/lib/components/code.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let selectable = false;
+</script>
+
+<code class:select-all={selectable} class="font-mono py-0 px-2 rounded-md bg-gray-2 dark:bg-gray-7 text-light-red dark:text-dark-red">
+	<slot />
+</code>
