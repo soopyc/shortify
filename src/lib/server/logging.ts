d930837adbb71c442f2f0f95d98d457767eb0ff0
Cassie Cheung <me@soopy.moe>
2024-06-26 21:58:01 +0800

    feat: handle logging levels via `DEBUG` env var

--- src/lib/server/logging.ts
+++ src/lib/server/logging.ts
@@ -2,15 +2,22 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { PUB_APP_NAME } from '$env/static/public';
 import { NODE_ENV } from "$env/static/private";
+import { env } from '$env/dynamic/private';
 import pino from "pino";
 
 export function getLogger(name: string) {
 	let transport = undefined;
+	let level = env.DEBUG || "debug"
+
 	if (NODE_ENV != "production") {
 		transport = pino.transport({
 			target: "pino-dev",
 		});
 	}
 
-	return pino({name: `${PUB_APP_NAME.toLowerCase()}:server:${name}`}, transport);
+	if (!level) {
+		level = "info" // default
+	}
+
+	return pino({name: `${PUB_APP_NAME.toLowerCase()}:server:${name}`, level}, transport);
 }
