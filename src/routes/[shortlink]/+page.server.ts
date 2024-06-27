33e0a4cbaf9984a3ae80d69dcfb73da30394b33e
Cassie Cheung <me@soopy.moe>
2024-06-28 00:51:39 +0800

    feat: add x-served-by header

--- src/routes/[shortlink]/+page.server.ts
+++ src/routes/[shortlink]/+page.server.ts
@@ -1,7 +1,9 @@
 import { redirect, error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
+import { PUB_APP_NAME } from "$env/static/public";
 
-export const load: PageServerLoad = () => {
+export const load: PageServerLoad = ({setHeaders}) => {
+	setHeaders({"X-Served-By": `${PUB_APP_NAME} v${APP_VER}`})
 	error(404)
 	// redirect()
 }
