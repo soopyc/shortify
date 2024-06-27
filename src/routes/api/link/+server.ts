48e03ce3b6fdb369c8a18deb96bbeaafa946957e
Cassie Cheung <me@soopy.moe>
2024-06-28 00:56:38 +0800

    feat: implement poc link generation

---
+++ src/routes/api/link/+server.ts
@@ -0,0 +1,52 @@
+import * as jose from "jose"
+import { getKey } from "$lib/server/jwk"
+import { json } from "@sveltejs/kit"
+import { generate } from "$lib/server/nanoid.js"
+import { PUB_APP_NAME, PUB_DOMAIN } from "$env/static/public"
+import postLink from "$lib/schemas/postLink.js"
+import { KEY_ALGO } from "$env/static/private"
+
+/**
+ * `GET /api/link`: provide information about a link
+ */
+export async function GET() {
+	throw new Error("Not implemented yet.")
+}
+
+/**
+ * `POST /api/link`: create a new link. should return an object with
+ *   - a shortlink
+ *   - a JWT that authenticates edits/deletions to the link
+ */
+export async function POST({ request }) {
+	let data;
+	try {
+		data = await request.json()
+	} catch (e) {
+		return json({ success: false, message: "invalid JSON data: JSON cannot be parsed." }, { status: 400 })
+	}
+	const parsed = postLink.safeParse(data)
+
+	if (!parsed.success) {
+		return json({success: false, message: "schema parse error, check data parameter for more details.", data: parsed.error.format()}, {status: 400})
+	}
+	
+	const { longLink, customLink, length } = parsed.data
+	// const _ = customLink
+	const shortId = generate(length)
+	return json({
+		shortId: shortId,
+		shortlink: `https://${PUB_DOMAIN}/${shortId}`,
+		key: await new jose.SignJWT({ patch: true, delete: true })
+			.setProtectedHeader({alg: KEY_ALGO})
+			.setSubject(shortId)
+			.setIssuer(`${PUB_APP_NAME} v${APP_VER}`)
+			.sign(getKey())
+	})
+}
+
+// export async function PATCH() {}
+
+export async function DELETE() {
+	throw new Error("Not implemented yet.")
+}
