9ca4447816c669febc87894a1f243ab1a399caf1
Cassie Cheung <me@soopy.moe>
2024-08-30 18:36:40 +0800

    fix: make shorthand endpoint propagate errors

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,14 +1,22 @@
-// replace this: take body as a string and return only a url. other things are in headers.
+// take body as a string and return only a url. other things are in headers.
+// essentially operating as a shorthand for the more longwinded api endpoint.
 export async function POST({ fetch, request, setHeaders }) {
-	const result = await (await fetch("/api/link", {
+	const result = await fetch("/api/link", {
 		method: "POST",
 		body: JSON.stringify({
 			longLink: await request.text()
 		})
-	})).json()
+	})
+	const finalData = await result.json()
 
-	setHeaders({ "x-modification-token": result.key })
-	return new Response(result.shortlink, { status: 201 }) // this must not return the same shortid if the same link is provided.
+	// check if response is successful
+	// if endpoint returns a 3xx something went wrong.
+	if (result.status >= 400) {
+		return new Response(`Failed to shorten link due to the following error: ${finalData.message}`, { status: result.status })
+	}
+
+	setHeaders({ "x-modification-token": finalData.key })
+	return new Response(finalData.shortlink, { status: 201 }) // this must not return the same shortid if the same link is provided.
 }
 
 // TODO
