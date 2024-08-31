af9a41e6fc69dfeb5c3cfce6ebabfa2e74c2e70d
Cassie Cheung <me@soopy.moe>
2024-09-01 00:42:47 +0800

    components: add extraClass to headign

--- src/lib/components/heading.svelte
+++ src/lib/components/heading.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	export let level = 1;
+	export let extraClass = {};
 </script>
 
-<div class="mt-3" class:text-3xl={level == 1} class:text-2xl={level == 2} class:text-xl={level == 3} {...$$restProps}>
+<div class="mt-3 {extraClass}" class:text-3xl={level == 1} class:text-2xl={level == 2} class:text-xl={level == 3} {...$$restProps}>
 	<slot />
 </div>
